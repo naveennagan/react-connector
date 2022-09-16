47626cf65bdb13455b7b9e78cb7962579e0d602d
Naveen Kumar Naganaboina <nnaganaboina@Naveens-MacBook-Pro.local>
2022-09-16 23:21:37 +0530

    basic setup

---
+++ src/lib/ReactConnectorEnd/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const ReactConnectorEnd = () => {
+
+    return <div className="react-connector-end">
+        React Connector End
+    </div>
+}
---
+++ src/lib/ReactConnectorStart/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const ReactConnectorStart = () => {
+
+    return <div className="react-connector-start">
+        React Connector Start
+    </div>
+}
